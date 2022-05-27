4f23490c8fc56e477d0e960e1c19665bbbfef70e
Roxanne Zhao <roxannezhao199@gmail.com>
2022-05-27 23:41:57 +0800

    First commit

---
+++ js/music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "In My Head - Arcando",
+    artist: "Arcando",
+    img: "img-1",
+    src: "music-1",
+  },
+  {
+    name: "On & On - Cartoon",
+    artist: "Cartoon",
+    img: "img-2",
+    src: "music-2",
+  },
+  {
+    name: "Find You There - Chenda & Shiah Maisel",
+    artist: "Chenda & Shiah Maisel",
+    img: "img-3",
+    src: "music-3",
+  },
+  {
+    name: "Feelings - Diviners & Azertion",
+    artist: "Diviners & Azertion",
+    img: "img-4",
+    src: "music-4",
+  }
+]
---
+++ js/script.js
@@ -0,0 +1,102 @@
+// select all required tags or element
+const wrapper = document.querySelector(".wrapper");
+musicImg = wrapper.querySelector(".img-area img");
+musicName = wrapper.querySelector(".song-details .name");
+musicArtist = wrapper.querySelector(".song-details .artist");
+mainAudio = wrapper.querySelector("#main-audio");
+playPauseBtn = wrapper.querySelector(".play-pause");
+prevBtn = wrapper.querySelector("#prev");
+nextBtn = wrapper.querySelector("#next");
+progressBar = wrapper.querySelector(".progress-bar");
+
+let musicIndex = 2;
+
+window.addEventListener("load", ()=>{
+  loadMusic(musicIndex); // calling load music function once window loaded
+})
+// load music function
+function loadMusic(indexNumb) {
+  musicName.innerText = allMusic[indexNumb - 1].name;
+  musicArtist.innerText = allMusic[indexNumb - 1].artist;
+  musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
+  mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+}
+
+// play music function
+function playMusic() {
+  wrapper.classList.add("paused");
+  playPauseBtn.querySelector("i").innerText = "pause";
+  mainAudio.play();
+}
+
+// pause music function
+function pauseMusic() {
+  wrapper.classList.remove("paused");
+  playPauseBtn.querySelector("i").innerText = "play_arrow";
+  mainAudio.pause();
+}
+
+//next music function
+function nextMusic() {
+  // increment of index by 1
+  musicIndex++;
+  // if musicIndex is greater than array length then musicIndex will be 1
+  musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+}
+
+//prev music function
+function prevMusic() {
+  musicIndex--;
+  musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+}
+
+//play or music button event
+playPauseBtn.addEventListener("click", ()=>{
+  const isMusicPaused = wrapper.classList.contains("paused");
+  isMusicPaused ? pauseMusic() : playMusic()
+});
+
+// next music btn event
+nextBtn.addEventListener("click", ()=>{
+  nextMusic();
+});
+
+// prev music btn event
+prevBtn.addEventListener("click", () => {
+  prevMusic();
+});
+
+// update progress bar width according to music current time
+mainAudio.addEventListener("timeupdate", (e)=>{
+  const currentTime = e.target.currentTime; // getting current time of song
+  const duration = e.target.duration; // getting total duration of song
+  let progressWidth = (currentTime / duration) * 100;
+  progressBar.style.width = `${progressWidth}%`;
+
+  let musicCurrentTime = wrapper.querySelector(".current");
+  musicDuration = wrapper.querySelector(".duration");
+
+  mainAudio.addEventListener("loadeddata", ()=>{
+    // update song total duration
+    let audioDuration = mainAudio.duration;
+    let totalMin = Math.floor(audioDuration / 60);
+    let totalSec = Math.floor(audioDuration % 60);
+    if(totalSec < 10){ // adding 0 if sec is less than 10
+      totalSec = `0${totalSec}`;
+    }
+    musicDuration.innerText = `${totalMin}:${totalSec}`;
+  });
+
+  // update playing song current time
+  let currentMin = Math.floor(currentTime / 60);
+  let currentSec = Math.floor(currentTime % 60);
+  if (currentSec < 10) { // adding 0 if sec is less than 10
+    currentSec = `0${currentSec}`;
+  }
+  musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+
+});
